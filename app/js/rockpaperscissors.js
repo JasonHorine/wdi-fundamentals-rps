fb9379e4ad32de374bcb6a68d4be8f9929781888
jason horine <jasonhorine@jasons-MacBook-Pro.local>
2016-01-16 21:42:27 -0500

    Added code inside function getWinner

--- app/js/rockpaperscissors.js
+++ app/js/rockpaperscissors.js
@@ -34,13 +34,49 @@ function getComputerMove(move) {
     // However, if `move` is not specified / is null, your expression should equal `randomPlay()`.
     return move || randomPlay();
 }
-
+// function compares playerMove and computerMove, returning 'computer', 'player', or 'tie'
+// function treats any input other than 'rock' or 'paper' as 'scissors'
 function getWinner(playerMove,computerMove) {
     var winner;
-    // Write code that will set winner to either 'player', 'computer', or 'tie' based on the values of playerMove and computerMove.
-    // Assume that the only values playerMove and computerMove can have are 'rock', 'paper', and 'scissors'.
-    // The rules of the game are that 'rock' beats 'scissors', 'scissors' beats 'paper', and 'paper' beats 'rock'.
-    /* YOUR CODE HERE */
+    switch (playerMove){
+        case 'rock':
+            switch (computerMove){
+                case 'rock':
+                    winner = 'tie';
+                    break;
+                case 'paper':
+                    winner = 'computer'
+                    break;
+                default:
+                    winner = 'player'
+            }
+            break;
+        case 'paper':
+            switch (computerMove){
+                case 'rock':
+                    winner = 'player';
+                    break;
+                case 'paper':
+                    winner = 'tie';
+                    break;
+                default:
+                    winner = 'computer';
+                    break;
+            }
+            break;
+        default:
+            switch (computerMove){
+                case 'rock':
+                    winner = 'computer';
+                    break;
+                case 'paper':
+                    winner = 'player';
+                    break;
+                default:
+                    winner = 'tie';
+                    break;
+            }
+    }
     return winner;
 }
 
