a128040a38cbac6af4d2927c37dd0bca7ea2b709
jason horine <jasonhorine@jasons-MacBook-Pro.local>
2016-01-27 20:27:03 -0500

    Added functions playToFive() and playToGoal(goal).  playToFive() simply calls playToGoal() and passes in 5 as the goal.

--- app/js/rockpaperscissors.js
+++ app/js/rockpaperscissors.js
@@ -45,10 +45,10 @@ function getWinner(playerMove,computerMove) {
                     winner = 'tie';
                     break;
                 case 'paper':
-                    winner = 'computer'
+                    winner = 'computer';
                     break;
                 default:
-                    winner = 'player'
+                    winner = 'player';
             }
             break;
         case 'paper':
@@ -80,12 +80,48 @@ function getWinner(playerMove,computerMove) {
     return winner;
 }
 
-function playToFive() {
+function playToFive() { // call playToCount and pass in 5 as number of wins to end game
+    return playToCount(5);
+}
+
+function playToCount(goal) {
     console.log("Let's play Rock, Paper, Scissors");
     var playerWins = 0;
     var computerWins = 0;
-    // Write code that plays 'Rock, Paper, Scissors' until either the player or the computer has won five times.
-    /* YOUR CODE HERE */
+    var computerMove = null;
+    var playerMove = null;
+    // Write code that plays 'Rock, Paper, Scissors' until either the player or the computer has won 'goal' times.
+    while (playerWins < goal && computerWins < goal){ // run this loop until computer or player gets 'goal' score
+        computerMove = getComputerMove(); // get rock, paper, or scissors for computer
+        playerMove = getPlayerMove();  // get rock, paper, or scissors for player
+        switch (getWinner(playerMove,computerMove)){ // determine winner, then increment score in switch
+            case 'computer': // if computer won the round, do this
+                computerWins++;
+                console.log('Computer\'s ' + computerMove + ' beats player\'s ' + playerMove + '!');
+                if (computerWins == goal){ // if computer has won the game, announce it.
+                    console.log('Computer WINS!');
+                }
+                else {  // if computer has not won, report score.
+                    console.log('Player advances.  The score is now:');
+                }
+                break;
+            case 'player':  // if player won the round, do this
+                playerWins++;
+                console.log('Player\'s ' + playerMove + ' beats computer\'s ' + computerMove + '!');
+                if (playerWins == goal){ // if player has won the game, announce it.
+                    console.log('Player WINS!');
+                }
+                else {  // if player has not won, report the score
+                    console.log('Player advances.  The score is now:');
+                }
+                break;
+            case 'tie':  // if tie, do this
+                console.log('Holy hand-jive, a tie!  Both players played ' + computerMove + '.');
+                console.log('The score is still:'); 
+                break;
+        }
+        console.log('Player: ' + playerWins + '\nComputer : ' + computerWins + '\n'); // print the score after the switch message
+    }
     return [playerWins, computerWins];
 }
 
